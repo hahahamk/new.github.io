6e04baee662dc45fcf1daa36f0ee4578143956ef
hahahamk <111485641+hahahamk@users.noreply.github.com>
2022-10-12 16:26:28 +0900

    footer

--- js/common.js
+++ js/common.js
@@ -1,16 +1,17 @@
 //common include 
 includeHTML();
-
-// gnb
-$(".service-gnb .gnb > li").bind("mouseenter focusin", function() {
-	$(this).addClass("on").siblings().removeClass("on");
-});
-$(".service-gnb .gnb > li").bind("mouseleave focusout", function() {
-	$(this).removeClass("on");
-});
-
+ 
 // 사업자정보
 function comInfo() {
 	var element = document.getElementById("com-adr");
 	element.classList.toggle("on");
 }
+
+$(function(){	
+	$(".service-gnb .gnb > li").bind("mouseenter focusin", function() {
+		$(this).addClass("on").siblings().removeClass("on");
+	});
+	$(".service-gnb .gnb > li").bind("mouseleave focusout", function() {
+		$(this).removeClass("on");
+	});
+})
