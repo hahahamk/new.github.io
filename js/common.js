4fd392b73959c3b05d343c003a2e1d41efd42941
hahahamk <111485641+hahahamk@users.noreply.github.com>
2022-10-12 16:44:59 +0900

    gnb

--- js/common.js
+++ js/common.js
@@ -1,17 +1,16 @@
 //common include 
 includeHTML();
- 
+
+// gnb
+// $(".service-gnb .gnb > li").bind("mouseenter focusin", function() {
+// 	$(this).addClass("on").siblings().removeClass("on");
+// });
+// $(".service-gnb .gnb > li").bind("mouseleave focusout", function() {
+// 	$(this).removeClass("on");
+// });
+
 // 사업자정보
 function comInfo() {
 	var element = document.getElementById("com-adr");
 	element.classList.toggle("on");
 }
-
-$(function(){	
-	$(".service-gnb .gnb > li").bind("mouseenter focusin", function() {
-		$(this).addClass("on").siblings().removeClass("on");
-	});
-	$(".service-gnb .gnb > li").bind("mouseleave focusout", function() {
-		$(this).removeClass("on");
-	});
-})
