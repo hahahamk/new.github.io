f9ca04d9ae33013f690a654b9bb92daeee694695
hahahamk <111485641+hahahamk@users.noreply.github.com>
2022-10-13 14:44:20 +0900

    모바일용 하단메뉴

--- js/common.js
+++ js/common.js
@@ -14,3 +14,41 @@ function comInfo() {
 	var element = document.getElementById("com-adr");
 	element.classList.toggle("on");
 }
+
+// 모바일 하단 바로가기
+var lastScrollTop = 0;
+var delta = 5;
+var fixBox = document.querySelector('.service-gnb-btm');
+var fixBoxHeight = fixBox.offsetHeight;
+var didScroll;
+//스크롤 이벤트 
+window.onscroll = function(e) {
+	didScroll = true;
+};
+
+//0.25초마다 스크롤 여부 체크하여 스크롤 중이면 hasScrolled() 호출
+setInterval(function(){
+	if(didScroll){
+		hasScrolled();
+		didScroll = false;
+	}
+}, 250);
+
+function hasScrolled(){
+	var nowScrollTop = window.scrollY;
+	if(Math.abs(lastScrollTop - nowScrollTop) <= delta){
+		return;
+	}
+	if(nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight){
+		//Scroll down
+		fixBox.classList.remove('show');
+	}else{
+		if(nowScrollTop + window.innerHeight < document.body.offsetHeight){
+			//Scroll up
+			fixBox.classList.add('show');
+		}
+	}
+	lastScrollTop = nowScrollTop;
+}
+
+
