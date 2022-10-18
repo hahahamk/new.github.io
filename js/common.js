035e575c36d76d2f2281369b33b5368c7574b255
hahahamk <111485641+hahahamk@users.noreply.github.com>
2022-10-18 14:24:12 +0900

    modal

--- js/common.js
+++ js/common.js
@@ -51,4 +51,30 @@ function hasScrolled(){
 	lastScrollTop = nowScrollTop;
 }
 
+//modal
+var btn = document.querySelectorAll("button.modal-custom-button");
+var modals = document.querySelectorAll('.modal-custom');
+var spans = document.getElementsByClassName("close-modal");
 
+for (var i = 0; i < btn.length; i++) {
+    btn[i].onclick = function (e) {
+        e.preventDefault();
+        modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
+    
+    spans[i].onclick = function () {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target.classList.contains('modal-custom')) {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
+}
