46ce708c5b9357678ff9641299f75fd5052c912f
hahahamk <111485641+hahahamk@users.noreply.github.com>
2022-11-17 16:53:00 +0900

    bottom bar

--- js/common.js
+++ js/common.js
@@ -1,14 +1,6 @@
 //common include 
 includeHTML();
 
-// gnb
-// $(".service-gnb .gnb > li").bind("mouseenter focusin", function() {
-// 	$(this).addClass("on").siblings().removeClass("on");
-// });
-// $(".service-gnb .gnb > li").bind("mouseleave focusout", function() {
-// 	$(this).removeClass("on");
-// });
-
 // 사업자정보
 function comInfo() {
 	var btmadr = document.querySelector('.com-adr');
