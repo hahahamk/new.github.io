d2eee2f2ecdd642f8b75ce5ac18a0d21b88e908f
hahahamk <111485641+hahahamk@users.noreply.github.com>
2023-01-11 10:27:06 +0900

    map

--- js/common.js
+++ js/common.js
@@ -125,3 +125,127 @@ function toggleAccordion(el) {
          target.classList.add('active');
    }  
 }
+
+//custom selectbox
+const selectBoxElements = document.querySelectorAll(".custom-select");
+
+function toggleSelectBox(selectBox) {
+  selectBox.classList.toggle("active");
+}
+
+function selectOption(optionElement) {
+  const selectBox = optionElement.closest(".custom-select");
+  const selectedElement = selectBox.querySelector(".selected-value");
+  selectedElement.textContent = optionElement.textContent;
+}
+
+selectBoxElements.forEach(selectBoxElement => {
+  selectBoxElement.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    const isOptionElement = targetElement.classList.contains("option");
+
+    if (isOptionElement) {
+      selectOption(targetElement);
+    }
+
+    toggleSelectBox(selectBoxElement);
+  });
+});
+
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  const isSelect = targetElement.classList.contains("custom-select") || targetElement.closest(".custom-select");
+
+  if (isSelect) {
+    return;
+  }
+
+  const allSelectBoxElements = document.querySelectorAll(".custom-select");
+
+  allSelectBoxElements.forEach(boxElement => {
+    boxElement.classList.remove("active");
+  });
+});
+
+const $ = document.querySelector.bind(document)
+const sheetContents = sheet.querySelector(".lycontents")
+const draggableArea = sheet.querySelector(".draggable-area")
+let sheetHeight // in vh
+
+const setSheetHeight = (value) => {
+  sheetHeight = Math.max(0, Math.min(100, value))
+  sheetContents.style.height = `${sheetHeight}vh`
+
+  if (sheetHeight === 100) {
+    sheetContents.classList.add("fullscreen")
+  } else {
+    sheetContents.classList.remove("fullscreen")
+  }
+}
+
+const setIsSheetShown = (isShown) => {
+  sheet.setAttribute("aria-hidden", String(!isShown))
+}
+
+// Hide the sheet when clicking the background
+sheet.querySelector(".dim").addEventListener("click", () => {
+  setIsSheetShown(false)
+})
+
+const isFocused = element => document.activeElement === element
+
+// Hide the sheet when pressing Escape if the target element
+// is not an input field
+window.addEventListener("keyup", (event) => {
+  const isSheetElementFocused =
+    sheet.contains(event.target) && isFocused(event.target)
+
+  if (event.key === "Escape" && !isSheetElementFocused) {
+    setIsSheetShown(false)
+  }
+})
+
+const touchPosition = (event) =>
+  event.touches ? event.touches[0] : event
+
+let dragPosition
+
+const onDragStart = (event) => {
+  dragPosition = touchPosition(event).pageY
+  sheetContents.classList.add("not-selectable")
+  draggableArea.style.cursor = document.body.style.cursor = "grabbing"
+}
+
+const onDragMove = (event) => {
+  if (dragPosition === undefined) return
+
+  const y = touchPosition(event).pageY
+  const deltaY = dragPosition - y
+  const deltaHeight = deltaY / window.innerHeight * 100
+
+  setSheetHeight(sheetHeight + deltaHeight)
+  dragPosition = y
+}
+
+const onDragEnd = () => {
+  dragPosition = undefined
+  sheetContents.classList.remove("not-selectable")
+  draggableArea.style.cursor = document.body.style.cursor = ""
+
+  if (sheetHeight < 25) {
+    setIsSheetShown(false)
+  } else if (sheetHeight > 75) {
+    setSheetHeight(100)
+  } else {
+    setSheetHeight(50)
+  }
+}
+
+draggableArea.addEventListener("mousedown", onDragStart)
+draggableArea.addEventListener("touchstart", onDragStart)
+
+window.addEventListener("mousemove", onDragMove)
+window.addEventListener("touchmove", onDragMove)
+
+window.addEventListener("mouseup", onDragEnd)
+window.addEventListener("touchend", onDragEnd)
